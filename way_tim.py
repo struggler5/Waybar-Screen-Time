0f318fc4ff9063e0c59a973512c82a04fc313fba
Vleju Dumitru <112560005+struggler5@users.noreply.github.com>
2025-10-01 23:19:22 +0300

    Update way_tim.py

--- way_tim.py
+++ way_tim.py
@@ -1,56 +1,34 @@
 #!/usr/bin/env python3
+
 import os 
+tm =  str(os.popen("scrntime -d 1").read()).split()
 
 
-def getCMD():
-    cmd1 = " last -R $username |grep  'down'"
-
-
-    out =  str(os.popen(cmd1).read()).split("\n")[0].split()
-
-    if int(out[4]) < 10:
-        s = f"{out[3]}  {out[4]}"
-    else:
-        s = f"{out[3]} {out[4]}"
-    fin_cmd =  " last -R $username |grep '"+s+"'|grep 'down'|awk ' {print $NF }'"
-    
-    return fin_cmd 
+date = [tm[0][-3:], tm[1][:2 ]]
+time = [tm[3], tm[4][:2]]
+bar = tm[5][7:-7]
 
 
 
-tm =  str(os.popen(getCMD()).read()).split()
 
-hoursL = []
-minutesL = []
-
-for hs in tm:
-    hoursL.append(int(f"{hs[1]}{hs[2]}"))
-    minutesL.append(int(f"{hs[4]}{hs[5]}"))
-
-H = sum(hoursL) + int(sum(minutesL)/60)
-
-M = sum(minutesL)%60
-
-
-ut =  str(os.popen("uptime").read()).split()
-ut = ut[2]
-ut = ut.split(":")
-H = H + int(ut[0]) 
-
-if(M + int(ut[1].replace(',','')) ) <= 60:
-    M = M + int(ut[1].replace(',','')) 
-else:
-    H = H + 1
+s = f"[ {date[0]} {date[1]}   {time[0]}:{time[1]} {bar} ]" 
 
 
 
 
 
-s = f"| {H}h  {M}m |"
+
+
 if __name__ == "__main__":
     output = {
         "text": s,
         "class":"custom-screen_time"
             }
-
     print(s)
+
+
+
+
+
+
+
